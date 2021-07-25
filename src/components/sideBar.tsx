e7a067ff1b673a532da9a44ed42aa0e7e6f776b2
Tushar-Agarwal <agarwaltushar35@gmail.com>
2021-07-26 04:12:31 +0530

    added Log out button in Side Bar Component

--- src/components/sideBar.tsx
+++ src/components/sideBar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "../components/Button/button"
 
 interface Props {
 }
@@ -6,8 +7,12 @@ interface Props {
 const SideBar : React.FC<Props> = (props) => {
   return (
      <>
+       <div >
         <div className="w-40 h-screen text-white bg-black">Side Bar</div>
+        <Button title="Log out"></Button>
+      </div>
      </>
+
   );
 };
 

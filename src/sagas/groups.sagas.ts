2990ee9887d8fed84e760b36181ac32b6a9ddb0a
Tushar-Agarwal <agarwaltushar35@gmail.com>
2021-08-11 20:05:50 +0530

    implemented debouncing feature using take latest and delay function of Redux Saga

--- src/sagas/groups.sagas.ts
+++ src/sagas/groups.sagas.ts
@@ -1,14 +1,15 @@
-import { debounce , call,put} from "@redux-saga/core/effects"
+import { takeLatest , call,put,delay} from "@redux-saga/core/effects"
 import { AnyAction } from "redux"
 import { GROUPS_QUERY_CHANGED, } from "../actions/actions.constants"
 import { groupQueryResultsAction } from "../actions/groups"
 import  {fetchGroups as fetchGroupAPI}  from "../api/groups"
 
 export function* fetchGroups(action : AnyAction) : Generator<any> {
+    yield delay(1000);
     const groupRes : any = yield call(fetchGroupAPI , { query : action.payload , status : "all-groups" })
     yield put(groupQueryResultsAction(groupRes.data.data,action.payload));
 }
 
 export function* watchGroupQueryChanged() {
-    yield debounce(1000,GROUPS_QUERY_CHANGED,fetchGroups);
+    yield takeLatest(GROUPS_QUERY_CHANGED,fetchGroups);
 } 

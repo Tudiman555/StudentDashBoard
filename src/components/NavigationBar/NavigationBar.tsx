f37b624e0fd2e5cdb2565335507be2370a11606f
Tushar-Agarwal <agarwaltushar35@gmail.com>
2021-08-13 19:36:41 +0530

    fixed some warnings

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -12,7 +12,8 @@ const NavigationBar : React.FC<Props> = (props) => {
             <header className="flex items-center w-full h-12 py-1 mb-px bg-nav">
                 <div className="flex pl-4 md:pl-8">
                 <img src={CorkLogo} alt="" className="w-9 h-9 "  />
-                <a href="#" className="px-3 text-2xl font-semibold text-white">CORK</a>
+               
+                <a href="/" className="px-3 text-2xl font-semibold text-white">CORK</a>
                 </div>
                 <SearchBar></SearchBar>
             </header>
